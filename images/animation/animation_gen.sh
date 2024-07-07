fe4b1326c69dbd658ad4e2741b956c7f6dbb18f6
EvergreenTheTree <EvergreenTheTree@users.noreply.github.com>
2024-07-07 00:52:55 -0500

    Update animation script and regenerate animation in lower resolution

--- images/animation/animation_gen.sh
+++ images/animation/animation_gen.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+# Extremely sketchy animation script.
+# usage: animation_gen.sh input.svg output.gif [width] [height]
+
 svg_frame() {
-	sed "s/stroke-dashoffset:0/stroke-dashoffset:$(printf "%f" "$2")/" "$1"
+	sed -E "s/stroke-dashoffset:.*?;/stroke-dashoffset:$(printf "%f" "$2");/" "$1"
 }
 
-filename="$1"
-width=1024
-height=1024
+input_fn="$1"
+output_fn="$2"
+width="${3:-1024}"
+height="${4:-1024}"
 dash_offset=0
 max_offset="11.1"
 nframes=60
 for ((n = 0; n < nframes; n++)); do
 	dash_offset=$(echo "scale=7; ($n / $nframes) * $max_offset" | bc)
 	echo "$n: $dash_offset"
-	svg_frame "$filename" "$dash_offset" | inkscape --pipe --export-area-page --export-width="$width" --export-height="$height" -o "$(printf "frame_%05d.png" "$n")"
+	svg_frame "$input_fn" "$dash_offset" | inkscape --pipe --export-area-page --export-width="$width" --export-height="$height" -o "$(printf "frame_%05d.png" "$n")"
 done
+
+magick convert -layers OptimizePlus -delay 2 frame_*.png -loop 0 "$output_fn"
+
+rm -f frame_*.png
